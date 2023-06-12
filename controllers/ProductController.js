22e8e28316641bff6108a878bcd480696d9b8bc8
githukudenis <githukudenis@gmail.com>
2023-06-12 12:54:40 +0300

    add image field to product request body

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -2,7 +2,7 @@ const AppError = require(`${__dirname}/../util/AppError`)
 const CatchAsync = require(`${__dirname}/../util/CatchAsync`)
 const Product = require(`${__dirname}/../models/ProductModel`)
 const multer = require('multer')
-const {mongo} = require("mongoose");
+const mongoose = require("mongoose");
 
 const multerStorage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -10,7 +10,7 @@ const multerStorage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         //product-9097783405454jk5d-89888090.jpeg
-        const id = mongo.ObjectId()
+        const id = new mongoose.Types.ObjectId()
         const ext = file.mimetype.split("/")[1]
         cb(null, `product-${id}-${Date.now()}.${ext}`)
     }
@@ -26,7 +26,7 @@ const multerFilter = (req, file, cb) => {
 
 const upload = multer({
     storage: multerStorage,
-    fileFilter: multerStorage
+    fileFilter: multerFilter
 })
 
 exports.UploadFilePhoto = upload.single('image')
@@ -64,22 +64,25 @@ exports.GetProductById = (req, res, next) => {
 }
 
 exports.AddNewProduct = CatchAsync(async (req, res, next) => {
+    const productInfo = { "image": req.file.filename, ...req.body}
+    await Product.create(productInfo)
     res
         .status(200)
         .json({
             status: 'success',
-            message: 'Add new product'
+            message: 'Added new product successfully'
         })
 })
 
-exports.DeleteProduct = (req, res, next) => {
+exports.DeleteProduct = CatchAsync(async (req, res, next) => {
+    await Product.findByIdAndDelete(req.params.id)
     res
         .status(200)
         .json({
             status: 'success',
             message: 'Delete product route'
         })
-}
+})
 
 exports.UpdateProduct = (req, res, next) => {
     res
