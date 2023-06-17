746865e754820c576f24662fb42ee43bf4473591
githukudenis <githukudenis@gmail.com>
2023-06-17 11:23:47 +0300

    handle jwt and duplication errors

--- controllers/ErrorController.js
+++ controllers/ErrorController.js
@@ -3,6 +3,31 @@ const handleCastError = (err) => {
     let message = `Invalid ${err.path}: ${err.value}`
     return new AppError(message, 400)
 }
+
+const handleValidationError = (err) => {
+    const errors = Object.values(err.errors).map(el => el.message).join(', ')
+    const message = `Invalid input data: ${errors}`
+    return new AppError(message, 400)
+}
+
+const {JsonWebTokenError, TokenExpiredError} = require("jsonwebtoken");
+
+const handleInvalidIdError = (err) => {
+    let message = `Invalid ${err.path}: ${err.value}`
+    return new AppError(message, 400);
+}
+
+const handleInvalidJWTError = (err) => {
+    return new AppError(`${err.message}`, 401)
+}
+const handleExpiredJWT = (err) => new AppError('Your token has expired! Please log in again', 401)
+
+const handleDuplicateFields = (err) => {
+    const dupFields = Object.keys(err.keyValue)
+    const message = `Duplicate ${dupFields}. Please use another value`
+    return new AppError(message, 400)
+}
+
 const sendDevError = (err, res) => {
     res
         .status(err.statusCode)
@@ -42,6 +67,18 @@ module.exports = (err, req, res, next) => {
         let error = err
         if (err.name === "CastError") {
             error = handleCastError(err)
+        }
+        if (err.name === 'ValidationError') {
+            error = handleValidationError(err)
+        }
+        if (err.name === 'JsonWebTokenError') {
+            error = handleInvalidJWTError(err)
+        }
+        if (err instanceof TokenExpiredError) {
+            error = handleExpiredJWT(err)
+        }
+        if (err.code === 11000) {
+            error = handleDuplicateFields(err)
         }
         sendProdError(error, res)
     }
