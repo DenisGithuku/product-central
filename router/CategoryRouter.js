00ec932a6d076fd0267002b57515dc5c5b1043ed
githukudenis <githukudenis@gmail.com>
2023-06-17 11:23:24 +0300

    require token before resource access

--- router/CategoryRouter.js
+++ router/CategoryRouter.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const {GetProductCategories, AddCategory} = require(`${__dirname}/../controllers/CategoryController`)
+const {RestrictTo, Protect} = require(`${__dirname}/../controllers/AuthController`)
 
 router
     .route("/")
-    .get(GetProductCategories)
-    .post(AddCategory)
+    .get(Protect, GetProductCategories)
+    .post(Protect, RestrictTo('admin'), AddCategory)
 
 module.exports = router
