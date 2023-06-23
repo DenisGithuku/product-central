81a1f336437ba3624ee964d74df20ddc81f83531
githukudenis <githukudenis@gmail.com>
2023-06-23 09:02:15 +0300

    get rid of mongo config in index

--- api/index.js
+++ api/index.js
@@ -14,22 +14,6 @@ const ReviewRouter = require(`${__dirname}/../router/ReviewRouter`)
 const CategoryRouter = require(`${__dirname}/../router/CategoryRouter`)
 const UserRouter = require(`${__dirname}/../router/UserRouter`)
 
-const dotenv = require('dotenv')
-dotenv.config({path: `${__dirname}/config.env`})
-const app = require(`${__dirname}/api/index`)
-const port = process.env.PORT || 3000
-const mongoose = require('mongoose')
-
-const db_uri = process.env.DB_URL.replace("<password>", process.env.DB_PASSWORD)
-mongoose.connect(db_uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(conn => {
-    console.log("DB connection established....")
-}).catch(err => {
-    console.log(`DB ERROR 💥: ${err}`)
-})
-
 if (process.env.NODE_ENV === 'development') {
     index.use(morgan('dev'))
 }
